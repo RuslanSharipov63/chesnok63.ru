19807c19ebc1f8d70fe29a74bf4db1ea9b5bd4df
RuslanSharipov63 <76771081+RuslanSharipov63@users.noreply.github.com>
2021-01-18 19:15:09 +0400

    Add files via upload

---
+++ adress.php
@@ -0,0 +1,28 @@
+<?php 
+
+include 'header.html';
+?>
+
+ <div class="menu"> 
+       <div class="prime"><a target="blank" href="index.html">Главная</a></div>
+	   <div class="about_us"><a target="blank" href="onas.php">О нас</a></div>
+	   <div class="assort"><a target="blank" href="assortiment.php">Ассортимент</a></div>
+	   <div class="contacts"><a target="blank" href="adress.php">Контакты</a></div>
+	</div>
+	
+	<div class="adress2">
+	
+	<p>
+	
+	  Наш офис продаж находится по адресу: <br> Самарская область, Красный Яр, ул. Центральная, 36<br>
+	  Это райцентр Красноярского района. Здесь хорошие подъездные пути, современная транспортная развязка. Продуманная логистика позволит вам быстро приехать из любой точки региона и без лишних задержек забрать товар.
+
+	</p>
+	</div>
+	
+	<div class="karta">
+	<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acec99c80d095848553b627b4d0558a6ddd3cc614fb98f8edfe89a9a32f77f973&amp;width=800&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>
+</div>
+<?php 
+include 'footer.html';
+?>
---
+++ assortiment.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link type="text/css" rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+
+<?php 
+
+include 'header.html';
+?>
+
+
+  <div class="menu"> 
+       <div class="prime"><a target="blank" href="index.html">Главная</a></div>
+	   <div class="about_us"><a target="blank" href="onas.php">О нас</a></div>
+	   <div class="assort"><a target="blank" href="assortiment.php">Ассортимент</a></div>
+	   <div class="contacts"><a target="blank" href="adress.php">Контакты</a></div>
+     
+	</div>
+	
+	
+	<h2>Мы предлагаем</h2>
+	
+	<div class="katalog">
+	    <ul>
+	        <li>Чеснок «Любаша» - семенной и продовольственный от 180 рублей. Минимальная партия 10 кг.</li>
+	    </ul>
+	    
+	</div>
+
+	
+<div class="fotoassort">
+	    <div class="items1"></div>
+	    <div class="items2"></div>
+	    <div class="items3"></div>
+	    <div class="items4"></div>
+	    <div class="items5"></div>
+	</div>
+	
+
+	
+	
+	<?php 
+
+include 'footer.html';
+?>
+
+</body>
+</html>
